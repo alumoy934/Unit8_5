012e463d3fccb3574f9a30642d861a834edeccf4
alumoy934 <alumoy934@ieselcaminas.org>
2021-02-01 20:42:02 +0100

    SortedArray exercise

---
+++ src/SortedArray.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortedArray {
+
+    private int [] array;
+    private int numElements;
+
+    public SortedArray(int capacity){
+
+        numElements = 0;
+        array = new int[capacity];
+
+        for (int i = 0; i< array.length; i++){
+            array[i] = -1;
+        }
+
+    }
+
+    public void put (int value){
+        array[numElements] = value;
+        numElements++;
+        Arrays.sort(array,0,numElements);
+    }
+
+    public void view_array (){
+       System.out.println(Arrays.toString(array));
+    }
+
+    public int getSize(){
+        return array.length;
+    }
+
+    public int getNumElements(){
+        return numElements;
+    }
+
+    public int getElementAt(int value){
+        return array[value-1];
+    }
+
+    public void removeElement (int pos) {
+        if (pos <= numElements) {
+            array[pos - 1] = array[pos];
+            removeElement(pos + 1);
+        }
+
+    }
+
+    public boolean isEmpty(){
+        if (numElements == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isFull(){
+        if (numElements == array.length){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean existElement(int value) {
+        boolean found = false;
+
+        for (int i = 0; i < numElements; i++) {
+            if (array[i] == value) {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    public static void main(String[] args) {
+
+        SortedArray v = new SortedArray(10);
+        v.put(5);
+        v.put(10);
+        v.put(35);
+        v.put(1);
+        v.put(25);
+        v.view_array();
+        System.out.println("The size of array is: " + v.getSize());
+        System.out.println("Number of elements inserted into array: " + v.getNumElements());
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("View element at position: ");
+        int position = in.nextInt();
+        System.out.println("Elment at position " + position + " is: " + v.getElementAt(position));
+
+        System.out.println();
+        v.removeElement(3);
+        v.view_array();
+        System.out.println("Number of elements inserted into array: " + v.getNumElements());
+        System.out.println("Array Empty: " + v.isEmpty());
+        System.out.println("Array Full: " + v.isFull());
+
+        System.out.println("Value 25 exist into array: " + v.existElement(25));
+        System.out.println("Value 20 exist into array: " + v.existElement(20));
+
+    }
+
+
+
+}//class
